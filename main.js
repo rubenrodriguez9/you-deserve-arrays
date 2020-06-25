a87264aba5861e48d6c09fcab02594c284aafdda
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-06-25 15:41:00 -0400

    not completed

--- main.js
+++ main.js
@@ -2,8 +2,43 @@
  * YOUR CODE HERE *
  ******************/
 
+function getFirstItemFrom(arr){
+  return arr[0]
+}
 
+function getLastItemFrom(arr){
+  return arr[arr.length-1]
+}
 
+function getIndex3(arr){
+  if(arr.length < 4){
+    return arr[arr.length-1]
+  } else return arr[3]
+}
+
+function isLongList(arr){
+  if(arr.length > 10){
+  return true}
+  else if(arr.length === 10){
+    return true
+  }else if(arr.length < 10){
+    return false
+  }
+}
+
+function firstItemIsNumber(arr){
+  if(arr[0] >= 0 || arr[0] < 0){
+    return true
+  } else if(typeof arr[0] === 'string'){
+    return false
+  } else if(arr[0] === true || arr[0] === false)
+  return false
+
+}
+
+function secondCharOfThirdString(arr){
+  
+}
 
 
 
